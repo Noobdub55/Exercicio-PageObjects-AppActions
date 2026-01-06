6fd1fb941a38e7375ea1e7463f4f34ed81c3981d
matheus lima <matheus19win@gmail.com>
2026-01-06 14:28:18 -0300

    criado dockerfile e gerada imagem docker

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    baseUrl: 'https://lojaebac.ebaconline.art.br/',
     "experimentalStudio": true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
